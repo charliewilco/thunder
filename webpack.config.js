a527b6628374d96cb314fc1c609b07b13f4b1663
charles peters <charlespeters42@gmail.com>
2015-08-17 16:11:49 -0700

    updating NPM stuff

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,12 @@
+var webpack = require('webpack');
+
 module.exports = {
   entry: './index.js',
   output: {
     filename: 'thunder.min.js',
     path: './dist/'
-  }
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({minimize: true})
+  ]
 };
