f13e4d3f90d1f35e96f3c9f406cdc1b5d0c51ecf
charles peters <charlespeters42@gmail.com>
2016-09-24 17:47:57 -0700

    es6ified everything!

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
-var test = require('./src/detection');
-var Thunder = require('./src/thunder-images');
-
-var webpSrc = 'data:image/webp;base64,UklGRiQAAABXRUJQVlA4IBgAAAAwAQCdASoBAAEAAwA0JaQAA3AA/vuUAAA=';
-
-test(webpSrc, function(e) {
-  if (e && e.type !== 'load') {
-    Thunder();
+class Thunder {
+  constructor () {
+    const webpSrc = 'data:image/webpbase64,UklGRiQAAABXRUJQVlA4IBgAAAAwAQCdASoBAAEAAwA0JaQAA3AA/vuUAAA='
+    this.test(webpSrc, e => {
+      if (e && e.type !== 'load') {
+        this.setter()
+      }
+    })
   }
-});
+
+  setter () {
+    const images = document.querySelectorAll('[data-thunder]')
+
+    for (let i = 0; i < images.length; ++i) {
+      const fallback = images[i].getAttribute('data-thunder')
+      images[i].setAttribute('src', fallback)
+    }
+  }
+
+  test (uri, cb) {
+    const image = new Image()
+
+    function addResult (event) {
+    // TODO: rethink this pattern
+    // if the event is from 'onload', check the see if the image's width is
+    // 1 pixel (which indiciates support). otherwise, it fails
+
+      const result = event && event.type === 'load' ? image.width === 1 : false
+
+      if (cb) {
+        cb(event)
+      }
+
+      return result
+    }
+
+    image.onerror = addResult()
+    image.onload = addResult()
+    image.src = uri
+
+    return image
+  }
+}
+
+export default new Thunder()
--- src/thunder-images.js
+++
@@ -1,9 +0,0 @@
-module.exports = function() {
-  var images = document.querySelectorAll('[data-thunder]'), i;
-  var thunderImages = images[0].getAttribute('data-thunder');
-
-  for (i = 0; i < images.length; ++i) {
-    var fallback = images[i].getAttribute('data-thunder');
-    images[i].setAttribute('src', fallback);
-  }
-};
